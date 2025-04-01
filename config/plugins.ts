a2cc031044576cb0fd342cb367003d5bcfa69985
Anmol1001 <anmolpokhrel101@gmail.com>
2025-04-01 17:06:41 +0545

    added key in plugin.ts file

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   'users-permissions': {
     config: {
-      jwtSecret: env('JWT_SECRET'),
+      jwtSecret: env('JWT_SECRET', "m5rgKN8CLFzMH8/jhqOd3A=="),
     },
   },
 });
