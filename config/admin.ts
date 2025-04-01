6c15ea65a846f47d85e460971d207f9d235d331b
Anmol1001 <anmolpokhrel101@gmail.com>
2025-04-01 17:15:51 +0545

    added some keys on admin file

--- config/admin.ts
+++ config/admin.ts
@@ -1,13 +1,13 @@
 export default ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'default'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'KWw2y9b39ZyzTacMxsuN+A=='),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT', 'default'),
     },
   },
   flags: {
